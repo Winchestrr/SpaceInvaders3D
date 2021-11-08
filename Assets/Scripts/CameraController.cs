3b2ceb9cdf00d5dc1eff25ceee84e61f1134df56
Jakub Nowak <jakub_nowak@interia.pl>
2021-11-08 22:50:14 +0100

    Added camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public PlayerController playerController;
+
+    private void Update()
+    {
+        CameraMove();
+    }
+
+    void CameraMove()
+    {
+
+    }
+}
