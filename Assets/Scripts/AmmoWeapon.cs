84e1c6daf49f0880eb905d01973fbeb7f4b358c0
Jakub Nowak <jakub_nowak@interia.pl>
2021-11-15 15:29:19 +0100

    Finished AmmoWeapon mechanics (without UI)

--- Assets/Scripts/AmmoWeapon.cs
+++ Assets/Scripts/AmmoWeapon.cs
@@ -4,24 +4,55 @@ using UnityEngine;
 
 public class AmmoWeapon : GunBase
 {
-    [SerializeField] protected int maxAmmo;
-    public int currentAmmo;
+    [SerializeField] protected int magazineSize;
+    [SerializeField] protected int allBullets;
     [SerializeField] protected int ammoCost;
+    public int bulletsLeft;
 
-    void OnEnable()
+    [SerializeField] protected float reloadTime;
+    [SerializeField] protected bool isMagazineEmpty;
+
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
+        }
+
+        if(isMagazineEmpty)
+        {
+            //reload allert in UI controller true
+        }
     }
 
     protected override void Shoot()
     {
-        if(currentAmmo > 0)
+        if(bulletsLeft > 0)
         {
-            currentAmmo -= ammoCost;
+            isMagazineEmpty = false;
+            bulletsLeft -= ammoCost;
 
             base.Shoot();
             GameObject shotBullet = Instantiate(bullet, gunEnd.position, transform.rotation);
             shotBullet.GetComponent<StandardBullet>().Launch(damage, bulletSpeed);
+
+            if(bulletsLeft == 0)
+            {
+                isMagazineEmpty = true;
+                //reload allert true
+            }
         }
+    }
+
+    IEnumerator Reload()
+    {
+        if(allBullets > 0)
+        {
+            yield return new WaitForSeconds(reloadTime);
+            allBullets -= magazineSize;
+            bulletsLeft = magazineSize;
+            isMagazineEmpty = false;
+        }
+        else { yield break; }
     }
 }
