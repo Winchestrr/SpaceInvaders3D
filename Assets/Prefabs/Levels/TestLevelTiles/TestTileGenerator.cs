4c98150295a9677a2f04e0365adee345561febf7
Jakub Nowak <jakub_nowak@interia.pl>
2021-12-04 19:55:53 +0100

    Finished tile level generator (1 bug)
    After first spawn of level, there is a single empty space behind the last tile. It never appears again later.

--- Assets/Prefabs/Levels/TestLevelTiles/TestTileGenerator.cs
+++ Assets/Prefabs/Levels/TestLevelTiles/TestTileGenerator.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class TestTileGenerator : MonoBehaviour
 {
-    private List<GameObject> currentLevelTiles = new List<GameObject>();
+    public List<GameObject> currentLevelTiles = new List<GameObject>();
 
     public GameObject[] subtiles;
     public GameObject levelTile;
+    public GameObject levelGO;
+    private GameObject levelClone;
 
     public int tileLength;
     public int floorWidth;
@@ -16,13 +18,20 @@ public class TestTileGenerator : MonoBehaviour
     private int noInOrder = 0;
     public int numberOfTiles;
 
-    private void Update()
+    public float levelSpeed;
+
+    private void Start()
     {
-        if(Input.GetKeyDown(KeyCode.G))
-        {
-            GenerateLevel();
-        }
+        levelClone = Instantiate(levelGO, Vector3.zero, Quaternion.identity);
+        GenerateLevel();
     }
+
+    private void Update()
+    { 
+        MoveLevel();
+    }
+
+    #region Generators
 
     void GenerateLevel()
     {
@@ -30,13 +39,17 @@ public class TestTileGenerator : MonoBehaviour
         {
             noInOrder += 2 * tileLength;
 
-            float tempZ = noInOrder - 1;
-            GameObject tempTile = Instantiate(levelTile, new Vector3(0, 0, tempZ), Quaternion.Euler(new Vector3(0, 0, 0)));
-            currentLevelTiles.Add(tempTile);
-            
-            GenerateFloor(tempTile);
-            GenerateWalls(tempTile);
+            GenerateTile(new Vector3(0f, 0f, noInOrder - 1));
         }
+    }
+
+    void GenerateTile(Vector3 position)
+    {
+        GameObject tempTile = Instantiate(levelTile, new Vector3(0, 0, position.z), Quaternion.Euler(new Vector3(0, 0, 0)), levelClone.transform);
+        currentLevelTiles.Add(tempTile);
+
+        GenerateFloor(tempTile);
+        GenerateWalls(tempTile);
     }
 
     void GenerateWalls(GameObject parent)
@@ -68,6 +81,25 @@ public class TestTileGenerator : MonoBehaviour
                     new Vector3((2 * j) - floorWidth + 1, 0, (2 * i) + noInOrder),
                     Quaternion.Euler(new Vector3(0, 0, 0)),
                     parent.transform);
+            }
+        }
+    }
+
+    #endregion
+
+    void MoveLevel()
+    {
+        for (int i = 0; i < currentLevelTiles.Count; i++)
+        {
+            currentLevelTiles[i].transform.Translate(0f, 0f, (levelSpeed - PlayerController.playerSpeedOut) * Time.deltaTime, Space.Self);
+
+            if (currentLevelTiles[i].transform.localPosition.z < -(tileLength * 2))
+            {
+                Destroy(currentLevelTiles[i]);
+                currentLevelTiles.RemoveAt(i);
+
+                //tu jest problem, gdzie dodaje jedn¹ "pust¹" p³ytkê za pierwszym razem (potem jest git)
+                GenerateTile(new Vector3(0f, 0f, tileLength * 2 * (numberOfTiles - 1)));
             }
         }
     }
