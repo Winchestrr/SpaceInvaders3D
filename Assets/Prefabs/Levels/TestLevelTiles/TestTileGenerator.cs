489a42566123d5f9d44c90b3fef7843fc992fbcb
Jakub Nowak <jakub_nowak@interia.pl>
2021-12-04 18:29:26 +0100

    Added level tiles to empty objects

--- Assets/Prefabs/Levels/TestLevelTiles/TestTileGenerator.cs
+++ Assets/Prefabs/Levels/TestLevelTiles/TestTileGenerator.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class TestTileGenerator : MonoBehaviour
 {
+    private List<GameObject> currentLevelTiles = new List<GameObject>();
+
     public GameObject[] subtiles;
+    public GameObject levelTile;
 
     public int tileLength;
     public int floorWidth;
@@ -26,30 +29,45 @@ public class TestTileGenerator : MonoBehaviour
         for (int x = 0; x < numberOfTiles; x++)
         {
             noInOrder += 2 * tileLength;
-            GenerateFloor();
-            GenerateWalls();
+
+            float tempZ = noInOrder - 1;
+            GameObject tempTile = Instantiate(levelTile, new Vector3(0, 0, tempZ), Quaternion.Euler(new Vector3(0, 0, 0)));
+            currentLevelTiles.Add(tempTile);
+            
+            GenerateFloor(tempTile);
+            GenerateWalls(tempTile);
         }
     }
 
-    void GenerateWalls()
+    void GenerateWalls(GameObject parent)
     {
         for(int i = 0; i < tileLength; i++)
         {
             for(int j = 0; j < wallHeight; j++)
             {
-                Instantiate(subtiles[Random.Range(0, subtiles.Length)], new Vector3(-floorWidth, (2 * j) + 1, (2 * i) + noInOrder), Quaternion.Euler(new Vector3(0, 0, 90)));
-                Instantiate(subtiles[Random.Range(0, subtiles.Length)], new Vector3(floorWidth, (2 * j) + 1, (2 * i) + noInOrder), Quaternion.Euler(new Vector3(0, 0, -90)));
+                Instantiate(subtiles[Random.Range(0, subtiles.Length)],
+                    new Vector3(-floorWidth, (2 * j) + 1, (2 * i) + noInOrder),
+                    Quaternion.Euler(new Vector3(0, 0, 90)),
+                    parent.transform);
+
+                Instantiate(subtiles[Random.Range(0, subtiles.Length)],
+                    new Vector3(floorWidth, (2 * j) + 1, (2 * i) + noInOrder),
+                    Quaternion.Euler(new Vector3(0, 0, -90)),
+                    parent.transform);
             }
         }
     }
 
-    void GenerateFloor()
+    void GenerateFloor(GameObject parent)
     {
         for (int i = 0; i < tileLength; i++)
         {
             for (int j = 0; j < floorWidth; j++)
             {
-                Instantiate(subtiles[Random.Range(0, subtiles.Length)], new Vector3((2 * j) - floorWidth + 1, 0, (2 * i) + noInOrder), Quaternion.Euler(new Vector3(0, 0, 0)));
+                Instantiate(subtiles[Random.Range(0, subtiles.Length)],
+                    new Vector3((2 * j) - floorWidth + 1, 0, (2 * i) + noInOrder),
+                    Quaternion.Euler(new Vector3(0, 0, 0)),
+                    parent.transform);
             }
         }
     }
