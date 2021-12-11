f73ce8d0f056f030868e5c1c425f14947f1a178b
braaaur <bukalajakub@gmail.com>
2021-12-11 09:26:32 +0100

    LevelController fixes
    Now should spawn and move lvl properly

--- Assets/Scripts/Systems/LevelController.cs
+++ Assets/Scripts/Systems/LevelController.cs
@@ -4,29 +4,30 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
-    public List<GameObject> currentLevelTiles = new List<GameObject>();
+    [SerializeField] private List<GameObject> currentLevelTiles = new List<GameObject>();
 
     public GameObject[] wallsSubtiles;
     public GameObject[] floorSubtiles;
     public GameObject levelTile;
-    public GameObject levelGO;
+    //public GameObject levelGO; //tego nie potrzebujesz, masz transform tego obiektu do którego jest podpiêty skrypt pod transform, a sam game object pod gameObject
 
-    public Transform destroyLine;
+    //public Transform destroyLine; // co to robi, skoro nic nie robi? :)
 
-    public int wallSubtileSize;
-    public int floorSubtileSize;
-    public int tileLength;
-    public int floorWidth;
-    public int wallHeight;
+    public float wallSubtileSize;
+    public float floorSubtileSize;
+    public float tileLength;
+    public float floorWidth;
+    public float wallHeight;
 
-    private int noInOrder = 0;
+    //private int noInOrder = 0; //niezbyt dobry pomys³
     public int numberOfTiles;
+    public int startIndex = -1;
 
     public float levelSpeed;
 
     private void Start()
     {
-        destroyLine = GameObject.Find("DestroyLine").transform;
+        //destroyLine = GameObject.Find("DestroyLine").transform;
         GenerateLevel();
     }
 
@@ -39,17 +40,22 @@ public class LevelController : MonoBehaviour
 
     void GenerateLevel()
     {
-        for (int x = 0; x < numberOfTiles; x++)
+        for (int x = startIndex; x < numberOfTiles; x++)
         {
-            noInOrder += wallSubtileSize * tileLength;
+            //noInOrder += wallSubtileSize * tileLength; //no nie za bardzo w ten sposób, a nawet jak, to nie mo¿esz odejmowaæ -1 na dole od tego
 
-            GenerateTile(new Vector3(0f, 0f, noInOrder - 1));
+            //Debug.Log("For x = " + x.ToString() + ", result = " + (x * (wallSubtileSize * tileLength)).ToString());
+
+            GenerateTile(new Vector3(0f, 0f, x * (wallSubtileSize * tileLength))); //najpierw generuj Tile w dobrych miejscach
         }
     }
 
-    void GenerateTile(Vector3 position)
+    void GenerateTile(Vector3 givenPosition)
     {
-        GameObject tempTile = Instantiate(levelTile, new Vector3(0, 0, position.z), Quaternion.Euler(new Vector3(0, 0, 0)), levelGO.transform);
+        Debug.Log("GenerateTile(" + givenPosition.ToString() + ")");
+
+        //GameObject tempTile = Instantiate(levelTile, new Vector3(0, 0, position.z), Quaternion.Euler(new Vector3(0, 0, 0)), transform); //po co tworzy znów nowy wektor?
+        GameObject tempTile = Instantiate(levelTile, givenPosition, Quaternion.Euler(new Vector3(0, 0, 0)), transform);
         currentLevelTiles.Add(tempTile);
 
         GenerateFloor(tempTile);
@@ -63,12 +69,14 @@ public class LevelController : MonoBehaviour
             for(int j = 0; j < wallHeight; j++)
             {
                 Instantiate(wallsSubtiles[Random.Range(0, wallsSubtiles.Length)],
-                    new Vector3(-floorWidth, (wallSubtileSize * j) + 1, (wallSubtileSize * i) + noInOrder),
+                    //new Vector3(-floorWidth, (wallSubtileSize * j) + 1, (wallSubtileSize * i) + noInOrder), //ten noInOrder trochê Ciê zdradzi³ tutaj
+                    new Vector3(-((floorWidth / 2) * floorSubtileSize), (wallSubtileSize * j) + 1, (wallSubtileSize * i)) + parent.transform.position,
                     Quaternion.Euler(new Vector3(0, 0, -90)),
                     parent.transform);
 
                 Instantiate(wallsSubtiles[Random.Range(0, wallsSubtiles.Length)],
-                    new Vector3(floorWidth, (wallSubtileSize * j) + 1, (wallSubtileSize * i) + noInOrder),
+                    //new Vector3(floorWidth, (wallSubtileSize * j) + 1, (wallSubtileSize * i) + noInOrder), //ten noInOrder trochê Ciê zdradzi³ tutaj
+                    new Vector3(((floorWidth / 2) * floorSubtileSize), (wallSubtileSize * j) + 1, (wallSubtileSize * i)) + parent.transform.position,
                     Quaternion.Euler(new Vector3(180, 0, 90)),
                     parent.transform);
             }
@@ -82,7 +90,8 @@ public class LevelController : MonoBehaviour
             for (int j = 0; j < floorWidth; j++)
             {
                 Instantiate(floorSubtiles[Random.Range(0, floorSubtiles.Length)],
-                    new Vector3((floorSubtileSize * j) - floorWidth + 1, 0, (floorSubtileSize * i) + noInOrder),
+                    //new Vector3((floorSubtileSize * j) - floorWidth + 1, 0, (floorSubtileSize * i) + noInOrder), //ten noInOrder trochê Ciê zdradzi³ tutaj
+                    new Vector3((floorSubtileSize * j) - (((floorWidth - 1) / 2f)  * floorSubtileSize), 0, (floorSubtileSize * i)) + parent.transform.position,
                     Quaternion.Euler(new Vector3(0, 0, 0)),
                     parent.transform);
             }
@@ -97,14 +106,15 @@ public class LevelController : MonoBehaviour
         {
             currentLevelTiles[i].transform.Translate(0f, 0f, (levelSpeed - PlayerController.playerSpeedOut) * Time.deltaTime, Space.Self);
 
-            if (currentLevelTiles[i].transform.localPosition.z < -(tileLength * 2))
+            //if (currentLevelTiles[i].transform.localPosition.z < -(tileLength * 2))
+            if (currentLevelTiles[i].transform.localPosition.z < (-1f + startIndex) * (tileLength * floorSubtileSize))
             //if (currentLevelTiles[i].transform.localPosition.z < destroyLine.transform.position.z)
             {
                 Destroy(currentLevelTiles[i]);
                 currentLevelTiles.RemoveAt(i);
 
                 //tu jest problem, gdzie dodaje jedn¹ "pust¹" p³ytkê za pierwszym razem (potem jest git)
-                GenerateTile(new Vector3(0f, 0f, tileLength * 2 * (numberOfTiles - 1)));
+                GenerateTile(new Vector3(0f, 0f, tileLength * floorSubtileSize * (numberOfTiles - 1)));
             }
         }
     }
