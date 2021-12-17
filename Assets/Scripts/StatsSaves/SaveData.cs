135b1b27269cdcdfc1a50c8b9913d8967662e8b1
Jakub Nowak <jakub_nowak@interia.pl>
2021-12-17 21:13:08 +0100

    Started settings

--- Assets/Scripts/StatsSaves/SaveData.cs
+++ Assets/Scripts/StatsSaves/SaveData.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 [System.Serializable]
 public static class SaveData
 {
+    //stats
     public static int score { get; set; }
     public static float roundTime { get; set; }
     public static int enemiesKilled { get; set; }
     public static int chosenShip { get; set; }
+
+    //settings
+    public static float musicLevel { get; set; }
+    public static float sfxLevel { get; set; }
+    public static string graphicsQuality { get; set; }
 
     //public SaveData()
     //{
